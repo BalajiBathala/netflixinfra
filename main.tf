1196a3a730e709b72b8368ad586fdd79b6ebac3b
BalajiBathala <vijayabalajib2@gmail.com>
2025-07-01 16:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+region = "us-east-2"
+}
 
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-05df0ea761147eda6"
+instance_type = "t2.medium"
+key_name = "balajib"
+vpc_security_group_ids = ["sg-0329ce25cafddb8d1"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
